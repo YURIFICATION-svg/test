ef767e502ecd9a1c42a5158d187bb171e4044486
MikaCicada <YURIFICATION@inha.edu>
2026-06-24 22:10:37 +0900

    Delete YURIFICATION-svg.github.io-main/YURIFICATION-svg.github.io-main/stockgame directory

--- YURIFICATION-svg.github.io-main/YURIFICATION-svg.github.io-main/stockgame/backend/database.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-DATABASE_URL = "sqlite:///./stockgame.db"   # 로컬 개발용; 배포 시 PostgreSQL URL로 교체 가능
-
-engine       = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base         = declarative_base()
