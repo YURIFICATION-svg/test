fe97b6cf242341051409e8c50344970fab82e3e9
MikaCicada <YURIFICATION@inha.edu>
2026-06-24 22:31:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function addComment() {
+  c = document.createElement('p');
+  c.innerHTML = document.getElementById('sendmessage').value;
+  c.className = "blue";
+	document.getElementById('chat').appendChild(c);
+  document.getElementById('sendmessage').value = ""
+}
 
+function getComment() {
+  c = document.createElement('p');
+  c.innerHTML = document.getElementById('getmessage').value;
+  c.className = "red";
+	document.getElementById('chat').appendChild(c);
+  document.getElementById('getmessage').value = ""
+}
