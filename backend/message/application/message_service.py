a9d37d443d98ed3f5c4ae091cbfc9e18eb66d08b
MikaCicada <YURIFICATION@inha.edu>
2026-08-27 23:14:53 +0900

    Update message_service.py

--- backend/message/application/message_service.py
+++ backend/message/application/message_service.py
@@ -1,20 +1,14 @@
-from ulid import ULID
-from dependency_injector.wiring import Provide
-from fastapi import Depends, HTTPException
-
 from message.domain.message import Message
 from message.domain.repositary.message_repo import IMRepositary
 from user.domain.repositary.user_repo import IUserRepositary
-from containers import Container
 
 
 class MService:
     def __init__(
         self,
-        message_repo: IMRepositary = Depends(Provide[Container.message_repo]),
-        user_repo: IUserRepositary = Depends(Provide[Container.user_repo]),
+        message_repo: IMRepositary,
+        user_repo: IUserRepositary,
     ):
-        self.ulid = ULID()
         self.message_repo = message_repo
         self.user_repo = user_repo
 
@@ -28,7 +22,7 @@ class MService:
         receiver = self.user_repo.find_by_name(receiver_name)
 
         message = Message(
-            id=self.ulid.generate(),
+            id=self._generate_id(),
             sender_id=sender_id,
             receiver_id=receiver.id,
             title=title,
@@ -46,3 +40,9 @@ class MService:
 
     def find_by_id(self, message_id: str):
         return self.message_repo.find(message_id)
+
+    @staticmethod
+    def _generate_id() -> str:
+        import ulid
+
+        return str(ulid.new())
