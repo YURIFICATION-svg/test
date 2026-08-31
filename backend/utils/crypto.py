8a4615cfc4b1aedcd8f1b8de83dc90de79fcaf7b
MikaCicada <YURIFICATION@inha.edu>
2026-08-31 23:19:38 +0900

    Update crypto.py

--- backend/utils/crypto.py
+++ backend/utils/crypto.py
@@ -5,8 +5,6 @@ class Crypto:
         self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def encrypt(self, secret):
-        print(secret)
-        print(self.pwd_context.hash(secret))
         return self.pwd_context.hash(secret)
 
     def verify(self, secret, hash):
